7418cb5120b6cd65dbb2a4630b1c0242df72f4c4
bhargavi-max <chinnibharagvi123@gmail.com>
2022-09-21 15:35:11 +0530

    Second Commit

--- __pycache__/Python_DSA/Queue.py
+++ __pycache__/Python_DSA/Queue.py
@@ -20,7 +20,7 @@ class Queue:
     def dequeue(self):
         if self.size > 0:
             item_remove=self.head
-            print(str(item_remove.get_value()) +"is removed")
+            print(str(item_remove.get_value()) +" is served")
             if self.get_size()==1:
                 self.head=None
                 self.tail=None
@@ -29,12 +29,12 @@ class Queue:
             self.size-=1
             return item_remove.get_value()
         else:
-            print("queue is empty you can't remove an element")
+            print("Empty")
     def peek(self):
         if self.size>0:
             return self.head.get_value()
         else:
-            print("Nothing to Show")
+            print("No more orders!!")
     def is_empty(self):
         return self.size==0
     def get_size(self):
@@ -47,10 +47,34 @@ class Queue:
                 print(a.get_value() , end=" ")
                 a=a.next_node
 
-q= Queue()
-q.enqueue(2)
-q.enqueue(3)
-q.enqueue(4)
-q.enqueue(5)
-print(q.peek())
-q.printqueue()
+
+print("Creating a deli line with up to 10 orders...\n------------")
+deli_line = Queue()
+print("Adding orders to our deli line...\n------------")
+deli_line.enqueue("egg and cheese on a roll")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+deli_line.enqueue("toasted sesame bagel with butter and jelly")
+deli_line.enqueue("toasted roll with butter")
+deli_line.enqueue("bacon, egg, and cheese on a plain bagel")
+deli_line.enqueue("two fried eggs with home fries and ketchup")
+deli_line.enqueue("egg and cheese on a roll with jalapeos")
+deli_line.enqueue("plain bagel with plain cream cheese")
+deli_line.enqueue("blueberry muffin toasted with butter")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+# ------------------------ #
+# Uncomment the line below:
+#deli_line.enqueue("western omelet with home fries")
+# ------------------------ #
+print("------------\n Our first order will be " + deli_line.peek())
+print("------------\n Now serving...\n------------")
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+print(deli_line.peek())
